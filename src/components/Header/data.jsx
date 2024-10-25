6c6d9536c606155ed7632aec3d62bfb4a0857dbe
aihamnedal <94713844+aihamnedal@users.noreply.github.com>
2024-10-25 00:05:03 -0500

    Add nav bar with sprint 1 requirements listed

---
+++ src/components/Header/data.jsx
@@ -0,0 +1,46 @@
+import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa';
+export const links = [
+  {
+    id: 2,
+    url: '/nutrition',
+    text: 'nutrition',
+  },
+  {
+    id: 3,
+    url: '/workout',
+    text: 'workout',
+  },
+  {
+    id: 4,
+    url: '/social',
+    text: 'social',
+  },
+  {
+    id: 5,
+    url: '/profile',
+    text: 'profile',
+  },
+];
+
+export const social = [
+  {
+    id: 1,
+    url: 'https://www.twitter.com',
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: 'https://www.twitter.com',
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: 'https://www.twitter.com',
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 4,
+    url: 'https://www.twitter.com',
+    icon: <FaBehance />,
+  },
+];
