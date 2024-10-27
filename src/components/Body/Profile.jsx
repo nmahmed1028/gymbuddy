584395d984005d360ce957a1a769339ff4453dbc
aihamnedal <94713844+aihamnedal@users.noreply.github.com>
2024-10-26 23:31:45 -0500

    Remove default value on edit profile button

--- src/components/Body/Profile.jsx
+++ src/components/Body/Profile.jsx
@@ -55,8 +55,7 @@ const Profile = () => {
                     <input
                     className="text-white shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
                     id="name"
-                    defaultValue="" // TODO lookup name
-                    value={name}
+                    value={name} // TODO lookup name
                     onChange={(e) => setName(e.target.value)}
                     />
                 </fieldset>
@@ -74,8 +73,7 @@ const Profile = () => {
                         <input
                             className="text-white shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-r-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
                             id="username"
-                            defaultValue="" // TODO lookup username
-                            value={username}
+                            value={username} // TODO lookup username
                             onChange={(e) => setUsername(e.target.value)}
                         />
                     </div>
