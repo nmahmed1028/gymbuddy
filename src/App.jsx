e1a84afd4dc326ff183e6852a9bb8f9804ed2da0
ddavis-4 <123651199+ddavis-4@users.noreply.github.com>
2024-10-28 13:53:40 -0500

    Moved CSS from App to Index

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import {useState, useEffect} from 'react'
-import './App.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from './components/Header/NavBar'
 import AuthProvider from "./hooks/AuthProvider.jsx";
